585add33f2f5e7bbd130524c1b5fe32639c7d011
Andrew <aklynovyy@gmail.com>
2016-07-12 21:02:12 +0300

    include page and module scripts

--- weather-stat/indexPage.js
+++ weather-stat/indexPage.js
@@ -1,82 +1,66 @@
-var weatherModule = require('./weatherModule.js');
+var keysModule = require('./weatherModule.js');
 
 module.exports = (function() {
 	
 	var getPage = function (params) {
-		return '<html>' + getPageHead() +
-		'<body>' + 
-		getPageHeader() + 
-		getMain(params) +
-		'</body></html>';
+		return '<html>' + 
+			getPageHead() +
+			'<body>' + 
+			getPageHeader() + 
+		// getMain(params) +
+			getPageContaainer() +
+			'</body></html>';
 	};
 	
-	var getPageHead = function () {
-		return '<head>' +
-		'<title>Weather Statistic</title>' +
-		'<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">' +
-		'</head>';
+	var getPageHead = function (params) {
+		return '<head><title>No way</title></head>';
 	};
 	
-	var getPageHeader = function () {
+	var getPageHeader = function (params) {
 		return '<header class="page-header">' +
-			'<div class="container">' +
-				'<h1>Weather Statistics</h1>' +
+	  	'<div class="container">' +
+	  		'<h1>Weather Statistics</h1>' +
 			'</div>'
 		'</header>';
 	};
 	
-	var getMain = function (params) {
-		return '<main><h1>Keys</h1>' +
-		getSearchForm() +
-		getAllKeysTable(params) +		
-		'</main>';
+	var getPageContaainer = function () {
+		return '<div class="container">' +
+    	'<h2>Search maximum value</h2>' +
+		    '<select id="select-param">' +
+		      '<option value="">Select option</option>' +
+		      '<option value="maxTemp">Maximum Temperature</option>' +
+		      '<option value="maxHum">Maximum Tumidity</option>' +
+		      '<option value="maxWF">Maximum wind Force</option>' +
+		    '</select>' + 
+	    '<button id="search-btn" class="btn btn-info">Search</button>' +
+	    '<button id="global-stat-btn" class="btn btn-warning">Statistic</button>' +
+	    '<h3 id="param-description"></h3>' +
+	    '<div id="total-table"></div>' +
+	    '<h4 id="total-statistic"></h4>' +
+	  '</div>';
 	};
+
+
 	
-	var getAllKeysTable = function (params) {
-		var data = getViewData(params);
-		if (!data.length) {
-			return 'Nothing found';
+	function search() {
+		var param = $("#select-param").val();
+		var searchArray = serachParam(param);
+		if (param == "maxTemp") {
+			var serachParamItem = "The maximum temperature in " 
+			+ searchArray.city + " " + searchArray.day + " january: " + searchArray.maxParam + "C";
+		} else if (param == "maxHum") {
+			var serachParamItem = "Maximum humidity in " 
+			+ searchArray.city + " " + searchArray.day + " january: " + searchArray.maxParam;
+		}  else if (param == "maxWF") {
+			var serachParamItem = "The maximum wind force in " 
+			+ searchArray.city + " " + searchArray.day + " january: " + searchArray.maxParam;
 		}	
-		var result = "<tr><td>Date</td><td>Id key</td><td>Worker Name</td><td>Customer Name</td><td>Appartment Number</td></tr>";
-		for(var i = 0; i < data.length; ++i) {
-			result += "<tr>" +
-			"<td>" + data[i].date + "</td>" +
-			"<td>" + data[i].id + "</td>" +
-			"<td>" + data[i].worker.name + "</td>" +
-			"<td>" + data[i].key.customer.name + "</td>" +
-			"<td>" + data[i].key.apartment.number + "</td>" +
-			"</tr>";
-		}
-		return '<table border="1">' + result + '</table>';
-	};
-	
-	var getSearchForm = function () {		
-		return '<form method="POST"><label>Select worker</label>' + getWorkersSelect(weatherModule.getAllWorkers()) +
-		'<label>Enter customer name</label><input type="text" name="customer_name"/><input type="submit"/></form>';
-	};
-	
-	var getWorkersSelect = function (workers) {
-		var options = '';
-		for (var i = 0; i < workers.length; ++i) {
-			options += '<option value="' + workers[i].id + '">' + workers[i].name + '</option>';
-		}
-		return (options.length) ? '<select name="worker_id">' + options + '</select>' : '';
-	};
-	
-	var getViewData = function (params) {		
-		console.log(params);
-		if (!params) {
-			return weatherModule.getAll();
-		} 
-		if (params.customer_name) {
-			return weatherModule.searchByCustomer(params.customer_name);
-		} else if (params.worker_id) {
-			return weatherModule.searchByWorker(params.worker_id);
-		} else {
-			return [];
-		}
+
+		document.getElementById("search-btn").click();
+		$("#param-description").html(serachParamItem);
 	}
-	
+
 	return {
 		getPage: getPage
 	};
--- weather-stat/weatherModule.js
+++ weather-stat/weatherModule.js
@@ -1,139 +1,194 @@
-module.exports = (function () {
-	var data = [
-		{
-			"id": 1,
-			"date": "2015-10-12 14:44:14",
-			"worker": {
-				"id": 1,
-				"name": "worker"
-			},			
-			"key": {
-				"id": 1,
-				"apartment": {
-					"id": 1,
-					"number": 5
-				},
-				"customer": {
-					"id": 1,
-					"name": "customer"
-				},
-				"name": "my key"
-			}			
-		},
-		{
-			"id": 2,
-			"date": "2015-14-12 14:44:14",
-			"worker": {
-				"id": 2,
-				"name": "worker 1"
-			},			
-			"key": {
-				"id": 1,
-				"apartment": {
-					"id": 1,
-					"number": 5
-				},
-				"customer": {
-					"id": 1,
-					"name": "customer 1"
-				},
-				"name": "my key"
-			}			
-		},
-		{
-			"id": 3,
-			"date": "2015-14-12 14:44:14",
-			"worker": {
-				"id": 1,
-				"name": "worker"
-			},			
-			"key": {
-				"id": 1,
-				"apartment": {
-					"id": 1,
-					"number": 5
-				},
-				"customer": {
-					"id": 1,
-					"name": "Mary"
-				},
-				"name": "my key"
-			}			
-		}
-	];
-	
-	var searchByCustomer = function (customerName) {		
-		// All coll stuff find here
-		var result = [];
-		var expression = new RegExp("^" + customerName + "(.*)")
-		for(var i = 0; i < data.length; ++i) {
-			if(data[i].key.customer.name.search(expression) > -1) {
-				result.push(data[i]);
-			}
-		}
-		return result;
-	};
-	
-	var searchByWorker = function (id) {		
-		var result = [];
-		for (var i = 0; i < data.length; ++i) {
-			if (data[i].worker.id === parseInt(id)) {
-				result.push(data[i]);
-			}
-		}
-		return result;
-	};
-	
-	var searchByDate = function (date) {
-		var result = [];
-		for(var i = 0; i < data.length; ++i) {
-			if(Date.parse(data[i].date) === Date.parse(date)) {
-				result.push(data[i]);
-			}
-		}
-		return result;
-	};
-	
-	var getAllWorkers = function () {
-		var result = [];
-		var workers = [];
-		for (var i = 0; i < data.length; ++i) {
-			workers.push(data[i].worker);
-		}
-		for (var i = 0; i < workers.length; ++i) {
-			var unique = true;
-			for (var j = 0; j < result.length; ++j) {
-				if (workers[i].id === result[j].id) {
-					unique = false;
-				}
-			}
-			if (unique) {
-				result.push(workers[i]);
-			}			
-		}
-		return result;
-	};
-	
-	var getAll = function () {
-		return data;
-	};
+function globalStatistic(){
+	var statiscitArray = [],
+			globStat = "<table class='table table-bordered table-striped'><thead><tr>" 
+			+ "<th>" + "<b>City</b>" + "</th>" 
+			+ "<th>" + "<b>Average Temperature</b>" + "</th>" 
+			+ "<th>" + "<b>Average Humidity</b>" + "</th>" 
+			+ "<th>" + "<b>Average Wind Force</b>" + "</th>" + "</tr></thead>",
+			paramStat = "",
+			totalAverTemp = 0, 
+			totalAverHum = 0, 
+			totalAverWF = 0, 
+			totalCount = 0;
 
-	return {
-		searchByCustomer: searchByCustomer,
-		searchByDate: searchByDate,
-		getAll: getAll,
-		getAllWorkers: getAllWorkers,
-		searchByWorker: searchByWorker
-	};
-})();
+	for (var i = 0; i < citiesArray.length; i++) {
 
-/*console.log(keysModule.searchByCustomer("customer 1"));
-console.log(keysModule.searchByDate("2015-10-12 14:44:14"));
-var session = keysModule.searchByDate("2015-10-12 14:44:14");
-console.log(new Date(session[0].date))
-var sessionDate = new Date(session[0].date);
-console.log("It way the " + (sessionDate.getDay() + 1) + " day of the week");
-console.log("UTC time: " + sessionDate.getUTCDate());
-console.log("Number of seconds from 01.01.1970: " + sessionDate.getTime());
+		var averTemp = 0, averHum = 0, averWF = 0, count = 0;	
 
-console.log( Math.round(((Date.now() - sessionDate.getTime()) / (3600 * 24))) + " days ago");*/
+		for (var j = 0; j < dataArray.length; j++) {
+			if (citiesArray[i] == dataArray[j].city) {						
+				averTemp += parseInt(dataArray[j].temperature);
+				averHum 	+= parseInt(dataArray[j].humidity);
+				averWF	+= parseInt(dataArray[j].RandomWindForce);
+				count++;
+				totalCount++;
+			}
+		}
+		totalAverTemp += averTemp;
+		totalAverHum  += averHum;
+		totalAverWF   += averWF;
+		averTemp = 	(averTemp / count).toFixed(2);
+		averHum  = 	(averHum  / count).toFixed(2);
+		averWF   = 	(averWF   / count).toFixed(2);
+		statiscitArray.push({
+				city: 		citiesArray[i],
+				averTemp: 	averTemp,
+				averHum:  	averHum,
+				averWF: 		averWF
+		});	
+	}
+	totalAverTemp = (totalAverTemp / totalCount).toFixed(2);
+	totalAverHum  = (totalAverHum  / totalCount).toFixed(2);
+	totalAverWF   = (totalAverWF	 / totalCount).toFixed(2);
+	
+	for (var i = 0; i < statiscitArray.length; i++) {
+		globStat += "<tr>" + "<td>" + statiscitArray[i].city + "</td>" 
+		+ "<td>" + statiscitArray[i].averTemp + "</td>" 
+		+ "<td>" + statiscitArray[i].averHum +"</td>" 
+		+ "<td>" + statiscitArray[i].averWF + "</td>" + "</tr>";
+	}			
+
+	globStat += "</table>"
+	paramStat = "<h3>Total Statistic:</h3>"
+		+ "<ul><li>Average Temperature: " + totalAverTemp + "C</li>"
+		+ "<li>Average Humidity: " + totalAverHum	+ "</li>"
+		+ "<li>Average Wind Force: " + totalAverWF +"</li></ul>";
+		$("#total-table").html(globStat);
+		$("#total-statistic").html(paramStat);
+
+	document.getElementById("global-stat-btn").click();
+}
+
+function serachParam(param){
+	var paramArray = [];
+	for (var i = 0; i < dataArray.length; i++) {
+		if (param == "maxTemp") {
+			paramArray.push({
+				day: dataArray[i].day,
+				city: dataArray[i].city,
+				temp:dataArray[i].temperature
+			});
+		} else if (param == "maxHum") {
+			paramArray.push({
+				day: dataArray[i].day,
+				city: dataArray[i].city,
+				hum:dataArray[i].temperature
+			});
+		} else if (param == "maxWF") {
+			paramArray.push({
+				day: dataArray[i].day,
+				city: dataArray[i].city,
+				wF:dataArray[i].temperature
+			});
+		}
+	}	
+
+	var maxParam = 0, 
+			searchDay = 0, 
+			searchCity = "";
+	for (var i = 0; i < paramArray.length; i++) {
+		if (param == "maxTemp") {
+			if (i != (paramArray.length - 1) && paramArray[i].temp > paramArray[i + 1].temp) {
+				maxParam   = paramArray[i].temp;
+				searchDay  = paramArray[i].day;
+				searchCity = paramArray[i].city;
+			}
+		} else if (param == "maxHum") {
+			if (i != (paramArray.length - 1) && paramArray[i].hum > paramArray[i + 1].hum) {
+				maxParam   = paramArray[i].hum;
+				searchDay  = paramArray[i].day;
+				searchCity = paramArray[i].city;
+			}
+		} else if (param == "maxWF") {
+			if (i != (paramArray.length - 1) && paramArray[i].wF > paramArray[i + 1].wF) {
+				maxParam   = paramArray[i].wF;
+				searchDay  = paramArray[i].day;
+				searchCity = paramArray[i].city;
+			}
+		}
+	}
+
+	return ({
+		maxParam: maxParam,
+		day: searchDay,
+		city: searchCity
+	});
+}
+
+	function RandomTemp() {
+		var random = Math.random();
+		if (random < 0.01) {
+			return -1;
+		} else if (random < 0.05) {
+			return 0;
+		} else if (random < 0.1) {
+			return (random * 100);
+		} else if (random < 0.5) {
+			return (random * 100 / 2);
+		} else {
+			return 27;
+		}
+	}
+
+	function RandomWindForce() {
+		var random = Math.random();
+		if (random < 0.01) {
+			return 27;
+		} else if (random < 0.05) {
+			return 18;
+		} else if (random < 0.1) {
+			return 12;
+		} else if (random < 0.5) {
+			return 8;
+		} else {
+			return 4;
+		}
+	}
+
+	function RandomHum(){
+		var random = Math.random();
+		if (random < 0.01) {
+			return 35;
+		} else if (random < 0.06) {
+			return 45;
+		} else if (random < 0.1) {
+			return 62;
+		} else if (random < 0.6)  {
+			return 75;
+		} else {
+			return 100;
+		} 
+	}
+
+	function RandomWind(){
+		var random = Math.random();
+		if (random < 0.01) {
+			return "SW";
+		} else if (random < 0.06) {
+			return "N";
+		} else if (random < 0.1) {
+			return "NW";
+		} else if (random < 0.6)  {
+			return "W";
+		} else {
+			return "S";
+		} 
+	}
+
+	var dataArray = [];
+	var citiesArray = ["Rivne", "Kvasilov", "Dubno", "Lytsk"];
+
+	for (var j = 0; j < citiesArray.length; j++) {
+		for (var i = 1; i <= 30; i++) {
+			
+			dataArray.push({
+				city: citiesArray[j],
+				day: i,
+				humidity:RandomHum(),
+				temperature: RandomTemp().toFixed(2),
+				directionOfTheWind: RandomWind(),
+				RandomWindForce: RandomWindForce()
+			});					
+		}
+	}
+// console.log(dataArray);
